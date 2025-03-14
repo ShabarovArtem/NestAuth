4cd4926b0d1db6be287d245d33fb9f764b8635ca
Artem Shabarov <shabarovartemy@yandex.ru>
2025-03-14 16:00:47 +0300

    Add method login and logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,16 +5,17 @@ import {
   HttpStatus,
   Post,
   Req,
+  Res,
 } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from '@/auth/dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   public constructor(
     private readonly authService: AuthService
-    // private readonly configService: ConfigService
   ) {}
 
   @Post('register')
@@ -23,47 +24,18 @@ export class AuthController {
     return this.authService.register(req, dto);
   }
 
-  // @Post('login')
-  // @HttpCode(HttpStatus.OK)
-  // public async login(@Req() req: Request, @Body() dto: LoginDto) {
-  //   return this.authService.login(req, dto);
-  // }
-  //
-  // @UseGuards(AuthProviderGuard)
-  // @Get('/oauth/callback/:provider')
-  // public async callback(
-  //   @Req() req: Request,
-  //   @Res({ passthrough: true }) res: Response,
-  //   @Query('code') code: string,
-  //   @Param('provider') provider: string
-  // ) {
-  //   if (!code) {
-  //     throw new BadRequestException('Не был предоставлен код авторизации.');
-  //   }
-  //
-  //   await this.authService.extractProfileFromCode(req, provider, code);
-  //
-  //   return res.redirect(
-  //     `${this.configService.getOrThrow<string>('ALLOWED_ORIGIN')}/dashboard/settings`
-  //   );
-  // }
-  //
-  // @UseGuards(AuthProviderGuard)
-  // @Get('/oauth/connect/:provider')
-  // public async connect(@Param('provider') provider: string) {
-  //   const providerInstance = this.providerService.findByService(provider);
-  //
-  //   return {
-  //     url: providerInstance.getAuthUrl(),
-  //   };
-  // }
-  //
-  // @Post('logout')
-  // @HttpCode(HttpStatus.OK)
-  // public async logout(
-  //   @Req() req: Request,
-  //   @Res({ passthrough: true }) res: Response
-  // ) {
-  //   return this.authService.logout(req, res);
-  // }
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  public async login(@Req() req: Request, @Body() dto: LoginDto) {
+    return this.authService.login(req, dto);
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  public async logout(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    return this.authService.logout(req, res);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,17 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AuthMethod, User } from '@prisma/__generated__';
 import { RegisterDto } from './dto/register.dto';
 import { PrismaService } from '@/prisma/prisma.service';
 import { UserService } from '@/user/user.service';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
+import { verify } from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -39,42 +43,42 @@ export class AuthService {
     return this.saveSession(req, newUser);
   }
 
-  // public async login(req: Request, dto: LoginDto) {
-  //   const user = await this.userService.findByEmail(dto.email);
-  //
-  //   if (!user || !user.password) {
-  //     throw new NotFoundException(
-  //       'Пользователь не найден. Пожалуйста, проверьте введенные данные'
-  //     );
-  //   }
-  //
-  //   const isValidPassword = await verify(user.password, dto.password);
-  //
-  //   if (!isValidPassword) {
-  //     throw new UnauthorizedException(
-  //       'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.'
-  //     );
-  //   }
-  //
-  //   return this.saveSession(req, user);
-  // }
-  //
-  // public async logout(req: Request, res: Response): Promise<void> {
-  //   return new Promise((resolve, reject) => {
-  //     req.session.destroy((err) => {
-  //       if (err) {
-  //         return reject(
-  //           new InternalServerErrorException(
-  //             'Не удалось завершить сессию. Возможно, возникла проблема с сервером или сессия уже была завершена.'
-  //           )
-  //         );
-  //       }
-  //       res.clearCookie(this.configService.getOrThrow<string>('SESSION_NAME'));
-  //       resolve();
-  //     });
-  //   });
-  // }
-  //
+  public async login(req: Request, dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email);
+
+    if (!user || !user.password) {
+      throw new NotFoundException(
+        'Пользователь не найден. Пожалуйста, проверьте введенные данные'
+      );
+    }
+
+    const isValidPassword = await verify(user.password, dto.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException(
+        'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.'
+      );
+    }
+
+    return this.saveSession(req, user);
+  }
+
+  public async logout(req: Request, res: Response): Promise<void> {
+    return new Promise((resolve, reject) => {
+      req.session.destroy((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException(
+              'Не удалось завершить сессию. Возможно, возникла проблема с сервером или сессия уже была завершена.'
+            )
+          );
+        }
+        res.clearCookie(this.configService.getOrThrow<string>('SESSION_NAME'));
+        resolve();
+      });
+    });
+  }
+
   public async saveSession(req: Request, user: User) {
     return new Promise((resolve, reject) => {
       req.session.userId = user.id;
